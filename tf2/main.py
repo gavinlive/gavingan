aac827be7744bfc29d5565662b04526df7bac138
Gavin Seegoolam <22893238+gavinlive@users.noreply.github.com>
2019-11-22 14:45:26 +0000

    minor

--- tf2/main.py
+++ tf2/main.py
@@ -25,7 +25,6 @@ from tensorflow.keras import layers
 tf.get_logger().setLevel('ERROR')
 #tf.logging.set_verbosity(tf.logging.INFO)
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 from layers import Generator, Discriminator
 from loss import discriminator_loss, generator_loss
